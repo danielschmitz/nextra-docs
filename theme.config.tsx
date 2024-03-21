be3cf9c16c31a15f9b1a635a62bfcbf45fc57d25
Daniel Schmitz <danieljfa@gmail.com>
2024-03-20 23:18:27 -0300

    Initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,6 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+const config: DocsThemeConfig = {};
 
-export default config
+export default config;
